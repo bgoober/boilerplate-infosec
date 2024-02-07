f9aafc87b6964826792a1434df8abe4080a0faf0
Ben <bgoober2@protonmail.com>
2024-02-07 14:01:51 -0500

    Add security middleware and server.js file, update routing and static file serving

--- myApp.js
+++ myApp.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require('express'); // require express for app routing
+const helmet = require('helmet'); // require helmet for security middleware
 const app = express();
 
 
@@ -10,49 +11,15 @@ const app = express();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
+const api = require('./server.js'); // require the server.js file
+app.use(express.static('public')); // serve static files
+app.use(helmet.hidePoweredBy()); // hide the X-Powered-By header so hackers can't exploit known vulnerabilities
+app.use(helmet.frameguard({action: 'deny'})) // deny iframe clickjacking
+app.use(helmet.xssFilter()); // prevent cross-site scripting (XSS) attacks
+app.disable('strict-transport-security'); // disable the strict-transport-security header
+app.use('/_api', api); // use the api router
+app.get("/", function (request, response) { // serve the index.html file
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
